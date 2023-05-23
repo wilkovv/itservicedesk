02ac541cf9e0057d9adcc4ac062e130885cc46d8
Wojciech Wilczek <259443@student.pwr.edu.pl>
2023-05-23 20:21:15 +0000

    link for switching user/servicemen login forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+users = User.create(
+    email: 'jan.kowalski@example.com',
+    password: 'password',
+    password_confirmation: 'password',
+    name: 'Jan',
+    surname: 'Kowalski'
+  )
+users = User.create(
+email: 'jan.dzban@example.com',
+password: 'password',
+password_confirmation: 'password',
+name: 'Jan',
+surname: 'Dzban'
+)
+users = User.create(
+    email: 'mojciech.milczek@google.zip',
+    password: 'password',
+    password_confirmation: 'password',
+    name: 'Mojciech',
+    surname: 'Milczek'
+    )
+users = User.create(
+    email: 'warol.kolski@example.com',
+    password: 'password',
+    password_confirmation: 'password',
+    name: 'Warol',
+    surname: 'Kolski'
+    )
+servicemen = Serviceman.create(
+    email: 'wojciech.wilczek@example.com',
+    password: 'password',
+    password_confirmation: 'password',
+    name: 'Wojciech',
+    surname: 'Wilczek',
+    category: 'Hardware'
+    )
+servicemen = Serviceman.create(
+    email: 'michau.dudek@example.com',
+    password: 'password',
+    password_confirmation: 'password',
+    name: 'Michau',
+    surname: 'Dudek',
+    category: 'Software'
+    )
